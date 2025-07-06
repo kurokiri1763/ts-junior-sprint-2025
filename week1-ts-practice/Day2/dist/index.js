d1627c121a0b4d21cf2d844197a6d975b9943526
kurokiri1763 <kurokiri1763@gmail.com>
2025-07-06 17:35:12 +0900

    Day2: ユーザー型定義と関数の型注釈を追加

---
+++ week1-ts-practice/Day2/dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+// 関数に型注釈をつける
+function greetUser(user) {
+    return `Hello! ${user.name}!(${user.age}歳)`;
+}
+// 実行用データ
+const user = {
+    name: "Alice",
+    age: 30,
+    isAdmin: false
+};
+console.log(greetUser(user));
---
+++ week1-ts-practice/Day2/src/index.ts
@@ -0,0 +1,21 @@
+// 2日目
+// ユーザー型を定義
+type User = {
+    name: string;
+    age: number;
+    isAdmin: boolean;
+}
+
+// 関数に型注釈をつける
+function greetUser(user: User): string {
+    return `Hello! ${user.name}!(${user.age}歳)`
+}
+
+// 実行用データ
+const user: User = {
+    name: "Alice",
+    age: 30,
+    isAdmin: false
+}
+
+console.log(greetUser(user))
